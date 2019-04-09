06cdf28dadc25c1ddcfa432f275051d40344b0c1
Mark Alexieff <mark.alexieff@outlook.com>
2019-04-08 21:48:12 -0700

    Complete version that works - even if it is ugly.  sure there is a better way.

--- C6-X6-2019/C6-X6-2019.cpp
+++ C6-X6-2019/C6-X6-2019.cpp
@@ -25,8 +25,8 @@ Hint - don't bother with tokens, just read into a string using >>.
 	Continues to ask for sentences until  "XXX" is entered as first word of sentence
 */
 
-vector<string> conjunctions;
-vector<string> nouns;
+vector<string> conjunctions;			// vectors to hold the vocab/parts of speech
+vector<string> nouns;					// they get initialized later on
 vector<string> verbs;
 vector<string> articles;
 
@@ -51,7 +51,7 @@ bool get_sentence(vector<string>& s) {
 			s.push_back(temp);
 		}
 		else if (ch == '\n') more_to_read = false;			//  added to handle sentences with whitespace at the end
-		if (temp == "XXX" && s.size() == 1) return false;
+		if (temp == "XXX" && s.size() == 1) return false;	// false on return means we are tired of playing
 
 	}
 	return true;							// no real meaning currently
@@ -70,6 +70,7 @@ bool output_sentence(vector<string>& s) {
 	}
 
 // Set of functions to check grammar type of string
+// we pass a reference to the vector containing the elements and check to see if s is in them
 
 bool is_noun(vector<string>& n,string s);
 bool is_verb(vector<string>& v,string s);
@@ -77,9 +78,6 @@ bool is_conjunction(vector<string>& c, string s);
 bool is_article(vector<string>& a, string s);
 bool is_full_stop(string s);
 bool is_sentence(vector<string>& s);
-
-
-
 
 int main()
 
@@ -89,13 +87,11 @@ try
 	vector<string> sentence_to_test;
 	bool ret_value{ true };
 	bool keep_going{ true };
-
-
-
+	   
 	// Initialize vectors
 
-	conjunctions.push_back("and");
-	conjunctions.push_back("or");
+	conjunctions.push_back("and");				// Initialize the vectors of parts of speech
+	conjunctions.push_back("or");				// likely there is a better way to do this
 	conjunctions.push_back("but");
 	
 	nouns.push_back("birds");
@@ -109,8 +105,7 @@ try
 	articles.push_back("the");
 
 	cout << "Welcome to the English Grammar Checker V1.0\n";
-
-
+	
 	// While Keep Going is True has not been entered //
 
 	while (keep_going) {
@@ -119,7 +114,12 @@ try
 		keep_going = get_sentence(sentence_to_test);
 		ret_value = output_sentence(sentence_to_test);
 
-		cout << "\nTesting part of grammat checking fucntions\n\n";
+		cout << "\nTesting part of grammar checking fucntions\n\n";
+
+		/*
+		note the cooler way of iterating though a vector using iterator
+		rather the indexes - point to note is that an iterator is a pointer
+		*/
 
 		
 		for (auto it = begin(sentence_to_test); it != end(sentence_to_test); ++it) {
@@ -132,7 +132,8 @@ try
 			else cout << "not in the Grammar!\n\n";
 		}
 
-		ret_value = is_sentence(sentence_to_test);
+		ret_value = is_sentence(sentence_to_test);  // call to the function that does all the work
+
 		output_sentence(sentence_to_test);
 		cout << " ---- ";
 		if (ret_value) cout << "is a sentence!\n\n";
@@ -140,45 +141,6 @@ try
 
 	}
 
-
-
-
-
-	/* Check for exit condition (Vector length is 1 and string 1 == XXX)
-		If Exit condition - Output goodbye msg and break out of While loop */
-	
-	// Intitiate the Vector of strings //
-
-	// Call Sentence Checking Function - pass vector of strings //
-
-	/* Check Return value - TRUE, output Sentence and Is OK  
-		If FALSE, output sentence and Is Not Okay */
-	
-	
-	/*
-	while (keep_going) {
-		cin >> temp_s;
-		cin >> temp_i;
-
-
-		// Check for duplicates //
-
-		for (int i = 0; i < Names.size(); i++) {
-			if (temp_s == Names[i].Name) {
-				keep_going = false;
-				error("Duplicate Names not allowed!\n");
-			}
-		}
-		if (temp_s == "NoName" && temp_i == 0) {
-			keep_going = false;
-			break;
-		}
-		tempNameScore = NameScore(temp_s, temp_i);
-		Names.push_back(tempNameScore);
-	}
-	for (int j = 0; j < Names.size(); j++) {
-		Names[j].show();
-	}								*/
 	keep_window_open();
 }
 catch (exception& e) {
@@ -192,7 +154,7 @@ catch (...) {
 	return 2;
 }
 
-
+/* stupid function to copy a vector from postion pos onwards */
 bool copy_sentence(vector<string>& s1, int pos, vector<string>& s2)
 {
 	int i{ 0 };
@@ -240,20 +202,17 @@ bool is_full_stop(string s) {
 
 bool is_sentence(vector<string>& temp_sentence)
 {
-	//vector<string> temp_sentence;
-	//temp_sentence.push_back("poop");
+	vector<string> temp_sentence2;  // apparently you don't have to initialize a vector to have storage allocated - 
+									// meaning you can pass as a reference
 
-	vector<string> temp_sentence2;
-	//temp_sentence2.push_back("poop2");
 
 	int cur_position{ 0 };
 	bool ret_value{ true };
-	int stupid{ 0 };
-	
+		
 	//check to see if ends in full stop
 	if (!is_full_stop(temp_sentence.back())) return false;
 
-	//check to see if starts with a noun (but to check if starts with article first)
+	//check to see if starts with a noun (but need to check if starts with article first)
 	if (is_article(articles, temp_sentence[cur_position])) {
 		cur_position++;														// look at next word
 		if (!is_noun(nouns, temp_sentence[cur_position])) return false;  //initial article not follow by a noun
@@ -264,18 +223,20 @@ bool is_sentence(vector<string>& temp_sentence)
 
 	if (!is_verb(verbs, temp_sentence[cur_position])) return false;  //noun not followed by a verb
 
-	cur_position++;  // have noun ot article+noun followed by verb, look at next word.
+	cur_position++;  // have noun or article+noun followed by verb, look at next word.
 
+	/* next bit checks to see if we are at the last word in the sentence - has to be a full stop*/
+
+	if (cur_position + 1 == temp_sentence.size() && is_full_stop(temp_sentence[cur_position])) return true;
+
+	/* otherwise it has to be a conjunction or it is not a sentence */
+		
 	if (is_conjunction(conjunctions, temp_sentence[cur_position])) {
 		ret_value = copy_sentence(temp_sentence,cur_position, temp_sentence2);
 		if (!is_sentence(temp_sentence2)) return false;
 		else return true;
 	}
-
+	/* if we get to here then we have something other than a conjunction following a verb */
 	return false;
-//	if (is_full_stop(temp_sentence[cur_position])) {
-//		stupid = temp_sentence.size();
-//		if(cur_position == stupid-1) return true;
-//	}
-//	return false;
+
 }
